258e941da2c40a4bac6299289ad475c963a00da4
Cooper <124405920+Madilyncooper@users.noreply.github.com>
2023-06-19 15:18:50 -0700

    Added the blog edit handlebars page and the get route to render the page. Got content for specific blog posts to render in title and content spaces

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogPostRoutes = require('./blogPostRoutes')
+const editPostRoutes = require('./editRoutes')
 
 router.use('/users', userRoutes);
 router.use('/blogAdd', blogPostRoutes);
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -72,7 +72,28 @@ router.get('/blogAdd', withAuth, (req, res) => {
   })
 })
 
+router.get('/dashboard/edits/:id',withAuth, async (req, res) => {
 
+  try{
+
+    const dbRes = await Blogs.findAll({
+      where: {
+        id: req.params.id,
+      }
+    });
+
+    const blogEdit = dbRes.map(blog => {
+      return blog.get({ plain: true });
+    });
+    res.render('editBlog', {
+      logged_in: req.session.logged_in,
+      name: req.session.name,
+      blogEdit
+    })
+  }catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 module.exports = router;
